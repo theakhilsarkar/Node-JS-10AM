b41d904fde4001b7eebc2dce2b5eb986e8eb3362
theakhilsarkar <akhilsodvadiya7@gmail.com>
2025-12-04 10:58:53 +0530

    ..

--- multer/server.js
+++ multer/server.js
@@ -3,6 +3,7 @@ import multer from "multer";
 import path from "path";
 import { fileURLToPath } from "url";
 import mongoose from "mongoose";
+import fs from "fs";
 
 const app = express();
 
@@ -21,12 +22,13 @@ const fileSchema = new mongoose.Schema({
 });
 const File = mongoose.model("File", fileSchema);
 
+// multer
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, uploadPath);
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
+    cb(null, Date.now() + path.extname(file.originalname)); //
   },
 });
 
@@ -42,6 +44,47 @@ app.post("/upload", upload.single("image"), (req, res) => {
   res.json({ message: "Image uploaded successfully", file });
 });
 
+// query - ?
+// param - :id
+// body - {}
+
+app.delete("/upload", async (req, res) => {
+  try {
+    const file = await File.findById(req.query.id);
+    console.log(file.filepath);
+    // remove from storage/folder
+    if (fs.existsSync(file.filepath)) {
+      console.log("entered in condition....");
+      fs.unlinkSync(file.filepath);
+    }
+    // remove from database
+    await File.findByIdAndDelete(req.query.id);
+    res.json({ message: "Book deleted" });
+  } catch (err) {
+    res.status(500).json({ message: "Book Not Deleted", err: err.message });
+  }
+});
+
 app.listen(3000, () => {
   console.log("server started....");
 });
+
+// text -> mongodb
+
+// mongodb - store only text & number
+// file - multer
+
+// post
+// get file -> store in own uploads folder -> store path in mongodb
+
+// update
+// 1. remove old image
+// 2. place new image
+// 3. update image path.
+
+// delete
+// 1. delete from database,
+// 2. delete from folder
+
+// title , author, price, coverimage
+//
