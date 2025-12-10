b84678c8717a315b05fc603c43c285817396af64
theakhilsarkar <akhilsodvadiya7@gmail.com>
2025-12-10 11:02:36 +0530

    ....

---
+++ cookies/server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+// user information stored in db
+const USER = { email: "admin@gmail.com", password: 123456 };
+
+// middleware to protect route
+const isAuthenticated = (req, res, next) => {
+  const auth = req.cookies.auth;
+  if (auth) {
+    next();
+  } else {
+    res.status(401).json({ message: "Please login first !!" });
+  }
+};
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  if (email == USER.email && password == USER.password) {
+    res.cookie("auth", true, { httpOnly: true, maxAge: 1000 * 60 * 60 });
+    res.json({ message: "Login Successfull !!" });
+  } else {
+    res.status(401).json({ message: "Invalid Credential !" });
+  }
+});
+
+app.get("/home", isAuthenticated, (req, res) => {
+  res.json({ message: "Home Page" });
+});
+
+app.listen(4000, () => {
+  console.log("server started !!");
+});
